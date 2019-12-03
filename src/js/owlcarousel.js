919ec96d2308e2fd5fd06b19cbfa1e1be9457eb2
Justyna Oleś <justynaoles88@gmail.com>
2019-12-03 19:52:56 +0100

    przygotowanie json do karuzeli + dots
    kolory dots takie jak foteli, na podstawie pliku json

--- src/js/owlcarousel.js
+++ src/js/owlcarousel.js
@@ -1,8 +1,45 @@
-$('.owl-carousel').owlCarousel({
+
+
+const url ='src/js/assets/chairs.json'
+const owl = document.querySelector('.owl-carousel');
+let colors;
+
+fetch(url).then(data=> data.json()).then( data => {
+
+    colors = data;
+
+    console.log(colors[0].color)
+
+    data.forEach(element => {
+        
+        const img = element.img;
+        const owlItem = document.createElement('div');
+        owlItem.classList.add('item');
+        owlItem.style.backgroundImage="url("+img+")";
+
+
+        owl.appendChild(owlItem);
+
+    });
+
+}).then( data => {
+
+    $('.owl-carousel').owlCarousel({
  
-    margin: 0,
-    nav: false,
-    items: 1,
-    dots: true,
- 
-})
+        margin: 0,
+        nav: false,
+        items: 1,
+        dots: true,
+    });
+
+    const owlDots = [...document.querySelectorAll(".owl-dot span")];
+
+
+    for (let i = 0; i<owlDots.length; i++) {
+
+        owlDots[i].style.backgroundColor= colors[i].color;
+    }
+
+  
+
+});
