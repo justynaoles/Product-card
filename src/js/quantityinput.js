685a605f58e7bfcafd838aa06657acb6d0dc6c59
Justyna Oleś <justynaoles88@gmail.com>
2019-12-04 11:53:19 +0100

    formatowanie ceny przy zwiększaniu/zmniejszaniu

--- src/js/quantityinput.js
+++ src/js/quantityinput.js
@@ -3,6 +3,10 @@ const itemsNumber = document.querySelector('.quantity__input--input');
 const btnIncrease = document.querySelector('.js--inputIncrease');
 const btnDecrease = document.querySelector('.js--inputDecrease');
 const maxValue = 5;
+const startPrice = document.querySelector('.summary__price--value').innerHTML;
+
+
+
 
 
 function increaseNumber(input, max) {
@@ -11,19 +15,81 @@ function increaseNumber(input, max) {
 
     if(inputValue  < max) {
 
+        let finalPrice = Number(startPrice);
+        finalPrice = finalPrice * (inputValue + 1);
+
+        let formattedPrice = finalPrice.toString();
+        let numberDecimal = formattedPrice.indexOf('.')
+        let priceUpToDecimal = formattedPrice.slice(0,numberDecimal);
+        let afterDecimal = formattedPrice.slice(numberDecimal+1);
+        let roundedPrice;
+
+        if(numberDecimal === -1) {
+
+          roundedPrice = formattedPrice+".00";
+        }
+
+        else {
+
+            if(afterDecimal.length < 2) {
+
+                roundedPrice=priceUpToDecimal+"."+afterDecimal+"0"
+    
+            }
+    
+            else {
+    
+                roundedPrice = formattedPrice;
+            }
+        }
+
+
+        document.querySelector('.summary__price--value').innerHTML = roundedPrice;
         const increasedValue = (inputValue + 1).toString()
         input.value = increasedValue;
+
     }
 }
 
 function decreaseNumber(input) {
 
+    const currentPrice = Number(document.querySelector('.summary__price--value').innerHTML)
     const inputValue = Number(input.value);
 
-    if(inputValue  > 0) {
+    if(inputValue  > 1) {
 
-        const increasedValue = (inputValue - 1).toString()
-        input.value = increasedValue;
+        const decreasedValue = (inputValue - 1).toString()
+        input.value = decreasedValue;
+        const updatedPrice = currentPrice - startPrice;
+
+        let formattedPrice = updatedPrice.toString();
+        let numberDecimal = formattedPrice.indexOf('.')
+        let priceUpToDecimal = formattedPrice.slice(0,numberDecimal);
+        let afterDecimal = formattedPrice.slice(numberDecimal+1);
+        let roundedPrice;
+
+        if(numberDecimal === -1) {
+
+          roundedPrice = formattedPrice+".00";
+        }
+
+        else {
+
+            if(afterDecimal.length < 2) {
+
+                roundedPrice=priceUpToDecimal+"."+afterDecimal+"0"
+    
+            }
+    
+            else {
+    
+                roundedPrice = formattedPrice;
+            }
+        }
+      
+
+        document.querySelector('.summary__price--value').innerHTML =roundedPrice;
+    
     }
 }
 
